eac7053b0d9bebf20cf2a9ce1cfd14488f2ec925
romss1 <romanberth@gmail.com>
2025-02-15 22:10:31 +0100

    add poppin font

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        popin: ['Poppins'],
+      }
     },
   },
   plugins: [],
